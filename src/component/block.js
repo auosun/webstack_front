2551df4f179c6079f997bb360893707cc24f0c80
auosun <jzysuzhou@gmail.com>
2022-06-20 00:01:42 +0800

    fix(src)：修复页面样式错误问题

--- src/component/block.js
+++ src/component/block.js
@@ -7,15 +7,15 @@ class SiteList extends React.Component {
     render() {
         return (
             <div className="row">
-                <div className="col-sm-3">
-                    {this.props.value.map(item => (
+                {this.props.value.map(item => (
+                    <div className="col-sm-3">
                         <div className="xe-widget xe-conversations box2 label-info"
                              onClick={() => window.open(item.url, '_blank')}
                              data-toggle="tooltip" data-placement="bottom" title=""
                              data-original-title={item.title}>
                             <div className="xe-comment-entry">
                                 <a className="xe-user-img">
-                                    <img src={item.icon} className="lozad img-circle" width="40" alt="" />
+                                    <img src={item.icon} className="lozad img-circle" width="40" alt=""/>
                                 </a>
                                 <div className="xe-comment">
                                     <a href="#" className="xe-user-name overflowClip_1">
@@ -25,8 +25,8 @@ class SiteList extends React.Component {
                                 </div>
                             </div>
                         </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
             </div>
         )
     }
@@ -74,7 +74,7 @@ class MenuTag extends FetchBase {
         } else {
             let site_map = new Map()
             site_items.forEach(function (element) {
-                if (!site_map.has(element['group'])){
+                if (!site_map.has(element['group'])) {
                     site_map.set(element['group'], []);
                 }
 
@@ -82,17 +82,18 @@ class MenuTag extends FetchBase {
             })
 
             return (
-                <div>
+                <>
                     {items.map(item => (
                         <>
                             <h4 className="text-gray">
-                                <i className="linecons-tag" style={{ "margin-right": "7px"}} id={item.name} />
+                                <i className="linecons-tag" style={{"margin-right": "7px"}} id={item.name}/>
                                 {item.name}
                             </h4>
                             <SiteList value={site_map.get(item.id) ? site_map.get(item.id) : []}/>
+                            <br />
                         </>
                     ))}
-                </div>
+                </>
             )
         }
     }
@@ -102,7 +103,7 @@ class MenuTag extends FetchBase {
 function Block() {
     return (
         <div>
-            <MenuTag />
+            <MenuTag/>
         </div>
     );
 }
