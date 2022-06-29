88a061d264293eb4ba90bc30c29a65c818ef557a
auosun <jzysuzhou@gmail.com>
2022-06-29 22:27:53 +0800

    feat(footer): 设置footer 保证footer位于websites下方

--- src/component/content/mainContent.js
+++ src/component/content/mainContent.js
@@ -1,6 +1,34 @@
 import React from "react";
 import Nav from "./nav";
 import Block from "./block";
+
+
+class Footer extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <footer className="main-footer sticky footer-type-1">
+                <div className="footer-inner">
+                    <div className="footer-text">
+                        &copy; 2017-2022
+                        <a href="../cn/about.html"><strong>WebStack</strong></a> design by <a
+                        href="https://www.viggoz.com" target="_blank" rel="noreferrer"><strong>Viggo</strong></a>
+                    </div>
+                    <div className="go-up">
+                        <a href="#" rel="go-top">
+                            <i className="fa-angle-up"></i>
+                        </a>
+                    </div>
+                </div>
+            </footer>
+        );
+    }
+}
+
 class MainContent extends React.Component {
 
     constructor(props) {
@@ -16,29 +44,12 @@ class MainContent extends React.Component {
     }
 
     render() {
+
         return (
             <div className="main-content">
-                <Nav environment={this.state.environment} setEnvironment={this.setEnvironment} />
-                <Block groups={this.props.groups} params={{env: this.state.environment}} />
-
-                <footer className="main-footer footer-type-1" style={{
-                    position:"fixed",
-                    bottom:"0",
-                    width: "100%" }}
-                >
-                    <div className="footer-inner">
-                        <div className="footer-text">
-                            &copy; 2017-2022
-                            <a href="../cn/about.html"><strong>WebStack</strong></a> design by <a
-                            href="https://www.viggoz.com" target="_blank" rel="noreferrer"><strong>Viggo</strong></a>
-                        </div>
-                        <div className="go-up">
-                            <a href="#" rel="go-top">
-                                <i className="fa-angle-up"></i>
-                            </a>
-                        </div>
-                    </div>
-                </footer>
+                <Nav environment={this.state.environment} setEnvironment={this.setEnvironment}/>
+                <Block groups={this.props.groups} params={{env: this.state.environment}}/>
+                <Footer/>
             </div>
         );
     }
