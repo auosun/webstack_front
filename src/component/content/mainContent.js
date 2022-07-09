d8d67e7ac8fef2ad0653b9f7f37a970141d7c955
auosun <jzysuzhou@gmail.com>
2022-07-09 15:16:29 +0800

    feat(nav): 增加配置环境后，该用户保持已选择的环境

--- src/component/content/mainContent.js
+++ src/component/content/mainContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Nav from "./nav";
 import Block from "./block";
+import cookie from "react-cookies";
 
 
 class Footer extends React.Component {
@@ -33,18 +34,20 @@ class MainContent extends React.Component {
 
     constructor(props) {
         super(props);
+
         this.state = {
-            environment: 'default'
+            environment: parseInt(cookie.load('web_env')) ? parseInt(cookie.load('web_env')) : 'default'
         }
+        console.log(this.state)
         this.setEnvironment = this.setEnvironment.bind(this)
     }
 
     setEnvironment(env) {
+        cookie.save('web_env', env)
         this.setState({environment: env})
     }
 
     render() {
-
         return (
             <div className="main-content">
                 <Nav environment={this.state.environment} setEnvironment={this.setEnvironment}/>
